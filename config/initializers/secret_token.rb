011608a7100db72e7a954da2130a33fb884fb59d
krongk <kenrome@gmail.com>
2013-11-10 14:22:25 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RainCms::Application.config.secret_key_base = '4806b4fd0dcac7eac39606e0bbd1d607f1872400ff0138f812fbc7ce876e2ff8245dabbc108504e7b75956051080ca458df77ffa96df46076ddc43786fc0f80c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RainCms::Application.config.session_store :cookie_store, key: '_rain-cms_session'
