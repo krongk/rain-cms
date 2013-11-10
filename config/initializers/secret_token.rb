65bd0e6cca629a7f02a20b5c3dcf5bc7543b26a6
krongk <kenrome@gmail.com>
2013-11-10 14:45:44 +0800

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-RainCms::Application.config.secret_key_base = '4806b4fd0dcac7eac39606e0bbd1d607f1872400ff0138f812fbc7ce876e2ff8245dabbc108504e7b75956051080ca458df77ffa96df46076ddc43786fc0f80c'
+RainCms::Application.config.secret_key_base = 'c7b49984d21f57a0b4f72f3a7d028e5a8a56367078cf52cade5447e2a00900b732030645018b99008649a73acb3f6e961f2e4ff297cda062ffada1c1a99b0f70'
