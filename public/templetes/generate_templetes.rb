060bb917b1e4716c7724ab4afa13577bea28cac3
krongk <kenrome@gmail.com>
2013-11-18 18:39:37 +0800

    Sfs

--- public/templetes/generate_templetes.rb
+++ public/templetes/generate_templetes.rb
@@ -75,8 +75,8 @@ class DataExtractor
     return if error_flag
 
     index_content = File.open(index_path).read
-
-    index_content = Iconv.conv "GBK//IGNORE", 'UTF-8', index_content
+    index_content.delete!("^\u{0000}-\u{007F}")
+    #index_content = Iconv.conv "GBK//IGNORE", 'UTF-8', index_content
     %W[head foot header footer].each do |s|
       if /<!--\s*\[\[#{s} start\]\]\s*-->(.*)<!--\s*\[\[#{s} end\]\]v-->/im =~ index_content#.force_encoding("utf-8")
         the_content = $1
