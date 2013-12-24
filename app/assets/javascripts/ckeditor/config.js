2a4d1628ea55c0a6d529b63f7ed121b283890872
krongk <kenrome@gmail.com>
2013-12-24 14:48:09 +0800

    Add sun

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,10 +1,7 @@
-/*
-encoding: utf-8;
-
+﻿/*
 Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+Copy from: root@AY1311051648483881f3Z:/usr/local/rvm/gems/ruby-2.0.0-p247/bundler/gems/ckeditor-077827e01390/app/assets/javascripts/ckeditorG
 For licensing, see LICENSE.html or http://ckeditor.com/license
-每次版本更新，记得重新在以下目录拷贝覆盖此文件：
-C:\RailsInstaller\Ruby2.0.0\lib\ruby\gems\2.0.0\gems\ckeditor-4.0.6\app\assets\javascripts\ckeditor
 */
 
 CKEDITOR.editorConfig = function( config )
@@ -12,7 +9,7 @@ CKEDITOR.editorConfig = function( config )
   // Define changes to default configuration here. For example:
   // config.language = 'fr';
   // config.uiColor = '#AADC6E';
-  
+
   /* Filebrowser routes */
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
   config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
@@ -22,7 +19,7 @@ CKEDITOR.editorConfig = function( config )
 
   // The location of a script that handles file uploads in the Flash dialog.
   config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
-  
+
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
   config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
 
@@ -31,16 +28,16 @@ CKEDITOR.editorConfig = function( config )
 
   // The location of a script that handles file uploads in the Image dialog.
   config.filebrowserImageUploadUrl = "/ckeditor/pictures";
-  
+
   // The location of a script that handles file uploads.
   config.filebrowserUploadUrl = "/ckeditor/attachment_files";
-  
+
   // Rails CSRF token
   config.filebrowserParams = function(){
     var csrf_token, csrf_param, meta,
         metas = document.getElementsByTagName('meta'),
         params = new Object();
-    
+
     for ( var i = 0 ; i < metas.length ; i++ ){
       meta = metas[i];
 
@@ -59,10 +56,10 @@ CKEDITOR.editorConfig = function( config )
     if (csrf_param !== undefined && csrf_token !== undefined) {
       params[csrf_param] = csrf_token;
     }
-    
+
     return params;
   };
-  
+
   config.addQueryString = function( url, params ){
     var queryString = [];
 
@@ -75,18 +72,18 @@ CKEDITOR.editorConfig = function( config )
 
     return url + ( ( url.indexOf( "?" ) != -1 ) ? "&" : "?" ) + queryString.join( "&" );
   };
-  
+
   // Integrate Rails CSRF token into file upload dialogs (link, image, attachment and flash)
   CKEDITOR.on( 'dialogDefinition', function( ev ){
     // Take the dialog name and its definition from the event data.
     var dialogName = ev.data.name;
     var dialogDefinition = ev.data.definition;
     var content, upload;
-    
+
     if (CKEDITOR.tools.indexOf(['link', 'image', 'attachment', 'flash'], dialogName) > -1) {
       content = (dialogDefinition.getContents('Upload') || dialogDefinition.getContents('upload'));
       upload = (content == null ? null : content.get('upload'));
-      
+
       if (upload && upload.filebrowser && upload.filebrowser['params'] === undefined) {
         upload.filebrowser['params'] = config.filebrowserParams();
         upload.action = config.addQueryString(upload.action, upload.filebrowser['params']);
@@ -111,17 +108,10 @@ CKEDITOR.editorConfig = function( config )
   //xj: Allow all classes for all allowed elements.
   //e.g <ul class="slicer">
   //config.extraAllowedContent = '*(*)';
-  // 用上面的方式会导致图片无法渲染，不知道为何。
+  // use "*(*)" will cause image not render correctly, do not known why?
+  
   config.extraAllowedContent = 'ul(*);li(*);span(*)';
   //config.extraAllowedContent = 'li(*)';
   //config.extraAllowedContent = 'span(*)';
 
-  //
-  //xj: Allow <i> tag.
-  //e.g. <i class="fa fa-book"></i>
-  //config.allowedContent = true;
-  //config.ProtectedTags = 'i' ;
-  //config.protectedSource.push( /<i[\s\S]*?\>/g ); //allows beginning <i> tag
-  //config.protectedSource.push( /<\/i[\s\S]*?\>/g ); //allows ending </i> tag
 };
-
