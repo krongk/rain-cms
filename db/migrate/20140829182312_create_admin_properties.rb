cf524a0f3885d6e3bb4ba26486b5c07046a09962
krongk <kenrome@gmail.com>
2014-08-30 04:01:24 +0800

    updage page indexs

--- db/migrate/20140829182312_create_admin_properties.rb
+++ db/migrate/20140829182312_create_admin_properties.rb
@@ -4,15 +4,15 @@ class CreateAdminProperties < ActiveRecord::Migration
       t.string :name, :null => false
     end
     add_index :admin_properties, :name, :unique => true
-  end
 
-  execute "insert into admin_properties (name) values ('头条')"
-  execute "insert into admin_properties (name) values ('推荐')"
-  execute "insert into admin_properties (name) values ('幻灯')"
-  execute "insert into admin_properties (name) values ('底部')"
-  execute "insert into admin_properties (name) values ('滚动')"
-  execute "insert into admin_properties (name) values ('加粗')"
-  execute "insert into admin_properties (name) values ('图片')"
-  execute "insert into admin_properties (name) values ('跳转')"
+    execute "insert into admin_properties (name) values ('头条')"
+    execute "insert into admin_properties (name) values ('推荐')"
+    execute "insert into admin_properties (name) values ('幻灯')"
+    execute "insert into admin_properties (name) values ('底部')"
+    execute "insert into admin_properties (name) values ('滚动')"
+    execute "insert into admin_properties (name) values ('加粗')"
+    execute "insert into admin_properties (name) values ('图片')"
+    execute "insert into admin_properties (name) values ('跳转')"
+  end
 end
 
