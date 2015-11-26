8ec1bd7b1ec59723437ab8397ee6b6831d12df0b
xuejiang <xuejiang.avvo@gmail.com>
2015-11-26 11:21:17 +0800

    fef

--- app/workers/sms_send_worker.rb
+++ app/workers/sms_send_worker.rb
@@ -3,7 +3,7 @@ class SmsSendWorker
 
   def perform(mobile_phone, content)
     status_id = SmsBao.send(ENV['SMS_BAO_USER'], ENV['SMS_BAO_PASSWORD'], mobile_phone, content)
-    puts "#{ENV[DOMAIN_NAME]} sms send to: #{mobile_phone} -> #{status_id}"
+    puts "#{ENV['DOMAIN_NAME']} sms send to: #{mobile_phone} -> #{status_id}"
     status_id
   end
 end
