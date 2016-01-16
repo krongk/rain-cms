6e1b5523cd6856499fd0f6c12210524e4f99c265
xuejiang <xuejiang.avvo@gmail.com>
2016-01-16 11:24:59 +0800

    update smsbao

--- lib/sms_bao.rb
+++ lib/sms_bao.rb
@@ -2,20 +2,24 @@ require 'digest/md5'
 require 'open-uri'
 
 module SmsBao
-  # SMS_STATUS = {0 => 'success', 
-  #   30 => 'password error',
-  #   40 => 'bad account',
-  #   41 => 'no money',
-  #   42 => 'account expired',
-  #   43 => 'IP denied',
-  #   50 => 'content sensitive',
-  #   51 => 'bad phone number',
-  # }
+  # $statusStr = array(
+  #   "0" => "短信发送成功",
+  #   "-1" => "参数不全",
+  #   "-2" => "服务器空间不支持,请确认支持curl或者fsocket，联系您的空间商解决或者更换空间！",
+  #   "30" => "密码错误",
+  #   "40" => "账号不存在",
+  #   "41" => "余额不足",
+  #   "42" => "帐户已过期",
+  #   "43" => "IP地址限制",
+  #   "50" => "内容含有敏感词"
+  # );
   def self.send(user_name, password, phones, content)
     result = nil
     password = Digest::MD5.hexdigest password
     begin
-      open("http://www.smsbao.com/sms?u=#{user_name}&p=#{password}&m=#{phones}&c=#{URI.escape(content)}") {|f|
+      #surl = "http://www.smsbao.com/sms?u=#{user_name}&p=#{password}&m=#{phones}&c=#{URI.escape(content)}"
+      surl = "http://api.smsbao.com/sms?u=#{user_name}&p=#{password}&m=#{phones}&c=#{URI.escape(content)}"
+      open(surl) {|f|
         f.each_line {|line| result = line}
       }
     rescue => ex
